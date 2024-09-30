e1a434af7c0a65c19688bf89c8fc24d26bfc9e41
elango <b.elang93@gmail.com>
2024-09-30 11:43:38 +0530

    migrate issue fix

--- db/migrations/20240531155652-alter-remove-table-replenishment_allocation.js
+++ db/migrations/20240531155652-alter-remove-table-replenishment_allocation.js
@@ -1,20 +1,31 @@
 "use strict";
+
 exports.up = async function up(queryInterface, Sequelize) {
   try {
-    console.log("Drop Table: ");
-    const tableDefinition = await queryInterface.describeTable("replenishment_allocation");
-
-    if (tableDefinition) {
-      return queryInterface.dropTable("replenishment_allocation");
-    } else {
-      return null
-    }
+    console.log("Attempting to drop table: replenishment_allocation");
+    await queryInterface.dropTable("replenishment_allocation");
+    console.log("Table dropped successfully.");
   } catch (err) {
-    console.log(err);
+    // Handle the error by logging it
+    console.log("Error during dropping table:", err.message);
   }
 };
 
-exports.down = function down(queryInterface) {
-  return queryInterface.dropTable("replenishment_allocation");
+exports.down = async function down(queryInterface, Sequelize) {
+  try {
+    // Add logic to recreate the table if needed
+    // Example:
+    await queryInterface.createTable("replenishment_allocation", {
+      // Define your table columns here
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      // Add other columns as necessary
+    });
+    console.log("Table replenishment_allocation created successfully.");
+  } catch (err) {
+    console.log("Error during creating table:", err.message);
+  }
 };
-
