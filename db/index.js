c48969ea4a81f6ddfa8f8aa85c24000fa8168601
Elango <b.elango93@gmail.com>
2024-10-19 21:53:26 +0530

    fix

--- db/index.js
+++ db/index.js
@@ -3,11 +3,14 @@ const config = require("../lib/config").database;
 
 const db = {};
 
-db.connection = new Sequelize(config.databaseUrl, {
+db.connection = new Sequelize(`${config.databaseUrl}?sslmode=disable`, {
 	dialect: 'postgres',
 	dialectOptions: {
-		ssl: config.ssl || false
-	},  
+		ssl: {
+		  require: false, // Ensure SSL is not required
+		  rejectUnauthorized: false // Bypass SSL validation
+		}
+	  }, 
 	pool: {
 		max: config.poolMax,
 		min: config.poolMin,
